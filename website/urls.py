98debd37eef8e5798baa831f1edaa2405d2b9fe3
Keir Jeffrey Velez <keirjeffreyvelez18@gmail.com>
2018-01-12 12:03:31 +0800

    Transfer all html files from local to django

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^program' , views.program, name='program'),
+    url(r'^research' , views.research, name='research'),
+    url(r'^outreach' , views.outreach, name='outreach'),
+    url(r'^news' , views.news, name='news'),
+    url(r'^contactus', views.contactus, name='contactus'),
+]
---
+++ website/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
+
+def index (request):
+	template = loader.get_template('website/index.html')
+	return HttpResponse (template.render({},request))
+
+def program(request):
+	template = loader.get_template('website/program.html')
+	return HttpResponse(template.render({}, request))
+
+def research(request):
+	template = loader.get_template('website/research.html')
+	return HttpResponse(template.render({}, request))
+
+def outreach(request):
+	template = loader.get_template('website/outreach.html')
+	return HttpResponse(template.render({}, request))
+
+def news(request):
+	template = loader.get_template('website/news.html')
+	return HttpResponse(template.render({}, request))
+
+def contactus(request):
+	template = loader.get_template('website/contactus.html')
+	return HttpResponse(template.render({}, request))
