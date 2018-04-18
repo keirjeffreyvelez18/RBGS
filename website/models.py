dfcd455d83ae67da93de745b343b7b3c44972a29
christanPlaza <christan.plaza@tekinbox.com>
2018-04-19 01:00:54 +0800

    research tweaks

--- website/models.py
+++ website/models.py
@@ -25,7 +25,7 @@ class ResearchTitle(models.Model):
 	research_title = models.CharField(null="", max_length=1000)
 	research_abstract = models.TextField(null="", max_length=50000)
 	research_author = models.CharField(null="", max_length=1000)
-	research_resources = models.CharField(null="", max_length=10000)
+	research_date = models.DateTimeField(null="")
 
 	def __str__(self):
 		return self.research_title+" by "+self.research_author
